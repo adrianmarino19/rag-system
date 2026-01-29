4821db1cb37ba9c75eb84ed3d1d29157979e653b
Adrian Marino <adrian.marino@ailylabs.com>
2026-01-29 11:23:39 +0100

    made it more SE. remember to put in functions functionalities that you might use later

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -4,13 +4,21 @@ from .search_utils import DEFAULT_SEARCH_LIMIT,  load_movies
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
+    preproc_query = preprocessing(query)
 
     for movie in movies:
-        if query in movie['title']:
+        preproc_title = preprocessing(movie['title'])
+        if preproc_query in preproc_title:
             results.append(movie)
             if len(results) >= limit:
                 break
     return results
+
+
+def preprocessing(text: str) -> str:
+    text = text.lower()
+    return text
+
 
 
 # Remember to use 'in' in Python. Much faster!
