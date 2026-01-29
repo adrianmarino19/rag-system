fc5b846e6bd4461f3d8632e4937c295facf2d7b0
Adrian Marino <adrian.marino@ailylabs.com>
2026-01-29 08:53:57 +0100

    improved and made better SE decisions

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -3,9 +3,8 @@
 import argparse
 import json
 
-# Save all movies in a dictionary
-with open("data/movies.json", "r") as file:
-    movies_dic = json.load(file)
+from lib.keyword_search import search_command 
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -20,15 +19,31 @@ def main() -> None:
         case "search":
             print(f"Searching for: {args.query}")
 
-            # Inneficient for loop to check every single title. 
-            for movie in movies_dic['movies']:
-                for word in movie['title'].split(' '):
-                    if word == str(args.query):
-                        print(movie['title'])
+            results = search_command(args.query)
+            for i, movie in enumerate(results, start=1):
+                print(f"{i}. {movie['title']}")
+
         case _:
             parser.print_help()
 
 if __name__ == "__main__":
     main()
 
+
+
 # it works but without caps lock!
+# perhaps put everything in Classes eh!
+# why did the guy separate his files like that, why I didn't.
+# What is an util. 
+
+# Perhaps I should build a project with them to understand their organization...
+# And finish git this weekend!
+
+
+# Que aprendimos?
+## Comienza a poner type hints en todas las functions. 
+## Separa variables. Imaginate que es una aplicacion. 
+
+
+# No entiendas:
+## os.path -> mas practica. 
---
+++ cli/lib/search_utils.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+DEFAULT_SEARCH_LIMIT = 5  
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
+
+# Save all movies in a dictionary
+def load_movies() -> list[dict]:
+    with open(DATA_PATH, "r") as file:
+        data = json.load(file)
+    return data['movies']
